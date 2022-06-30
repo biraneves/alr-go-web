23e6a4a877918047187d8d5e81021c5b8757d819
biraneves <ubirajara.neves@usp.br>
2022-06-30 09:42:03 -0300

    Criada a função Insert para tratar o formulário de novo produto

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
+
 	"github.com/biraneves/alr-go-web/models"
 )
 
@@ -18,5 +21,36 @@ func Index(w http.ResponseWriter, r *http.Request) {
 func New(w http.ResponseWriter, r *http.Request) {
 
 	templ.ExecuteTemplate(w, "New", nil)
+
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		fltPreco, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+
+			log.Println("Erro na conversão do preço:", err)
+
+		}
+
+		intQuantidade, err := strconv.Atoi(quantidade)
+		if err != nil {
+
+			log.Println("Erro na conversão da quantidade:", err)
+
+		}
+
+		models.NovoProduto(nome, descricao, fltPreco, intQuantidade)
+
+	}
+
+	http.Redirect(w, r, "/", 301)
 
 }
