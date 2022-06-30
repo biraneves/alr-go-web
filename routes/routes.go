ac8147e61f30e7497396dc85b6ed764e7c67a7f5
biraneves <ubirajara.neves@usp.br>
2022-06-30 11:26:58 -0300

    Criada a rota para update

--- routes/routes.go
+++ routes/routes.go
@@ -12,5 +12,6 @@ func LoadRoutes() {
 	http.HandleFunc("/insert", controllers.Insert)
 	http.HandleFunc("/delete", controllers.Delete)
 	http.HandleFunc("/edit", controllers.Edit)
+	http.HandleFunc("/update", controllers.Update)
 
 }
