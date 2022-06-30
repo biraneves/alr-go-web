07fe508bd5fd30eefd57aae1755ff850704757b9
biraneves <ubirajara.neves@usp.br>
2022-06-30 11:38:40 -0300

    Criada a função Update

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -68,3 +68,34 @@ func Edit(w http.ResponseWriter, r *http.Request) {
 	produto := models.EditaProduto(idProduto)
 	templ.ExecuteTemplate(w, "Edit", produto)
 }
+
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		intId, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversão do Id:", err)
+		}
+
+		fltPreco, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		intQuantidade, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.AtualizaProduto(intId, nome, descricao, fltPreco, intQuantidade)
+
+	}
+
+}
